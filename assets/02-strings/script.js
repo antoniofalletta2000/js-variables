6d8ae9e3bcacd1b8810202cbc61007f6d35a6388
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-01-30 12:36:08 +0100

    complete second task first and second exercise

--- assets/02-strings/script.js
+++ assets/02-strings/script.js
@@ -1,11 +1,21 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
+const name="Antonio"
+console.log(name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const number="7"
+console.log(number);
+
+const text="Ciao"
+console.log(text);
+
+const bool="true"
+console.log(bool);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
